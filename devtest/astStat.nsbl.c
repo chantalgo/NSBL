b573c08a84dc4b366c739672636453124e1d1892
Jing <jing@Jing03.(none)>
2012-04-06 21:17:45 -0400

    codeGen CP1; stable

---
+++ devtest/astStat.nsbl.c
@@ -0,0 +1,2 @@
+int i_i0 = 0 ;
+float s_f4 = 0.1 ;
---
+++ devtest/astVarDecl.nsbl.c
@@ -0,0 +1,12 @@
+int i_i0 , j_i1 , k_i2 = 2 ;
+int p_i3 = i_i0 ;
+string s_s4 = "abcd" ;
+{ float a_f5 = (float) 10 , b_f6 = (float) 30 , i_f7 ;
+ s_s4 = "" ;
+ { (float) j_i1 + i_f7 ;
+ int p_i8 ;
+ } p_i3 = 1 ;
+ a_f5 += (float) 1 ;
+ }k_i2 += 2 ;
+{ j_i1 *= 2 ;
+ }
