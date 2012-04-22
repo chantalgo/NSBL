3f341652eb7ebe4ef9a0f058346dd44a5a2cc98a
Chantal Galvez <chantalgo.github@gmail.com>
2012-04-21 21:30:04 -0400

    [File I/O] Readgraph, Writegraph
    + print elements in graph (many printing functions are on derivedtype)

---
+++ src/NSBLio.c
@@ -0,0 +1,30 @@
+#include <string.h>
+#include <stdlib.h>
+#include "NSBLio.h"
+
+//function to print string
+void printString(char* str)
+{
+	printf("%s",str);
+}
+
+//function to print float
+void printFloat(float f)
+{
+	printf("%f",f);
+}
+
+//function to print integer
+void printInteger(int i)
+{
+	printf("%d",i);
+}
+
+
+//function to break line(newline)
+void printFromNewLine()
+{
+	printf("\n");
+}
+
+//graph, vertex and edge printing functions to be used from graph lib created by Lixing.
---
+++ src/NSBLio.h
@@ -0,0 +1,23 @@
+#ifndef NSBLIO_H_NSBL
+#define NSBLIO_H_NSBL
+
+#include <stdio.h>
+#include <string.h>
+#include "Derivedtype.h"
+
+//function to print string
+void printString(char* str);
+
+//function to print float
+void printFloat(float f);
+
+//function to print integer
+void printInteger(int i);
+
+//graph, vertex and edge printing functions to be used from graph lib created by Lixing.
+
+//function to break line(newline)
+void printFromNewLine();
+
+
+#endif
