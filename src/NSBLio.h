86de150c55997c2738d88c1fc27412f9b3784ccb
kunalmanch <kunalmanch@gmail.com>
2012-04-22 10:29:08 -0300

    Included graph, edge, vertex  print functions from graph lib

--- src/NSBLio.h
+++ src/NSBLio.h
@@ -14,10 +14,18 @@ void printFloat(float f);
 //function to print integer
 void printInteger(int i);
 
-//graph, vertex and edge printing functions to be used from graph lib created by Lixing.
-
 //function to break line(newline)
 void printFromNewLine();
+
+//graph, vertex and edge printing functions to be used from graph lib. 
+
+/*print functions for graph, 
+edge and vertex*/
+int print_g(GraphType* g);
+int print_v(VertexType* v);
+int print_e(EdgeType* e);
+int print_v_attr(VertexType* v);
+int print_e_attr(EdgeType* e);
 
 
 #endif
